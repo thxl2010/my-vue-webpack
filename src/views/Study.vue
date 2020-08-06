83eba4ea8d9def31ec2a45afeb36a20074a9a84f
Duyb <1255248666@qq.com>
2020-08-06 18:05:19 +0800

    Add: vue@3

---
+++ src/views/Study.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>count: {{ count }}</p>
+    <button @click="increase">increase</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const count = ref(0);
+    const increase = () => {
+      count.value++;
+    };
+
+    return {
+      count,
+      increase,
+    };
+  },
+};
+</script>
+
+<style></style>
