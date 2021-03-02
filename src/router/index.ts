3150c735b41b61279f84e0dd4b7c8a46cc7bf733
Duyb <1255248666@qq.com>
2021-03-02 15:14:39 +0800

    Add: first component with vue@3 & ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,32 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/study',
+    name: 'Study',
+    component: () =>
+      import(/* webpackChunkName: "study" */ '../views/Study.vue'),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Study.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>vue@3 & TypeScript ：</h3>
+  <ItemList :items="items" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ItemList from '@/components/items/ItemList.vue';
+
+export default defineComponent({
+  components: {
+    ItemList,
+  },
+  setup() {
+    const items: any[] = [
+      {
+        id: '1',
+        name: 'DU',
+      },
+      {
+        id: '2',
+        name: 'Li',
+      },
+      {
+        id: '3',
+        name: 'Chen',
+      },
+    ];
+
+    return {
+      items,
+    };
+  },
+});
+</script>
+
+<style></style>
